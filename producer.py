6cad35481c5d0c49ecc699567aab76a95c50b605
vishal8202 <111052930+vishal8202@users.noreply.github.com>
2022-11-28 03:04:16 +0530

    msg

--- producer.py
+++ producer.py
@@ -1,1 +1,11 @@
+from kafka import KafkaProducer
 
+bootstrap_server=["localhost:9092"]
+topic="Test"
+producer=KafkaProducer(bootstrap_servers=bootstrap_server)
+producer=KafkaProducer()
+data="Gods own country"
+message=producer.send(topic,bytes(data,"utf-8"))
+metadata = message.get()
+print(metadata.topic)
+print(metadata.partition)
